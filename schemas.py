dd69f3b83208f4a404057bb4d547b010445ba6f6
Liam Morales <dredknives04@gmail.com>
2025-05-29 22:28:00 +0800

    Modified modesl.py to include device parameter

--- schemas.py
+++ schemas.py
@@ -4,4 +4,7 @@ from typing import Dict
 class PredictionResponse(BaseModel):
     predicted_class: str
     confidence: float
-    all_confidences: Dict[str, float]
+    description: str
+    properties: List[str]
+    color: str
+    all_confidences: Dict[str, Dict[str, Union[float, str]]]
