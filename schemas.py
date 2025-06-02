e08487d4a50205a71f5b5614c49a0cc9b73a1a35
Liam Morales <dredknives04@gmail.com>
2025-06-03 00:10:07 +0800

    Added new endpoint for fertilizer prediction as well as the new model

--- schemas.py
+++ schemas.py
@@ -8,3 +8,29 @@ class PredictionResponse(BaseModel):
     properties: List[str]  # Now List is properly imported
     color: str
     all_confidences: Dict[str, Dict[str, Union[float, str]]]
+        
+        
+
+class FertilizerRequest(BaseModel):
+    Temperature: float
+    Humidity: float
+    Moisture: float
+    Soil_Type: str
+    Crop_Type: str
+    Nitrogen: float
+    Potassium: float
+    Phosphorous: float
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "Temperature": 25,
+                "Humidity": 60,
+                "Moisture": 25,
+                "Soil_Type": "Loamy",
+                "Crop_Type": "Maize",
+                "Nitrogen": 20,
+                "Potassium": 30,
+                "Phosphorous": 15
+            }
+        }
